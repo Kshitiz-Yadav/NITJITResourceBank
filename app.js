d93964245cdc7a3e115e3319cbd2e74e612e530d
Kshitiz <kshitizyadavofficial@gmail.com>
2023-01-29 01:03:58 +0530

    created partials for header and footer

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const path = require("path")
+const hbs = require("hbs")
 const express = require("express");
 const app = express()
 
 const staticPath = path.join("public")     
 app.use(express.static(staticPath))
-
 app.set('view engine', "hbs");
+
+const partialPath = "partials"
+hbs.registerPartials(partialPath);
 
 app.get("", (req,res)=>{
     res.render("loginPage")
 })
-
 app.get("/home", (req,res)=>{
     res.render("index.hbs")
+})
+app.get("*", (req,res)=>{
+    res.render("404.hbs")
 })
 
 app.listen(8000, ()=>{

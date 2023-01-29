69481789841e571e3a9e1e0bf366d272ce60c918
munna0912 <mrthalor912@gmail.com>
2023-01-30 00:25:03 +0530

    added Time-table, updated faculty contact feature

--- public/js/main.js
+++ public/js/main.js
@@ -118,3 +118,20 @@ function show_contact(element_id1,element_id2){
         document.getElementById(element_id2).style.display = "block";
     }
 }
+
+
+// Showing Model Image
+
+var modal = document.getElementById("myModal")
+var modalImg = document.getElementById("img01");
+function model_img(src)
+{
+    modal.style.display = "block";
+    modalImg.src = src;
+}
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}

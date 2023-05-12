ca3f20a69de0feb7cbeda331a4be3c126f20fd82
Kshitiz <kshitizyadavofficial@gmail.com>
2023-05-13 02:42:58 +0530

    Added gitignore

--- src/db/conn.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require("mongoose")
-mongoose.connect("mongodb+srv://resourcebankit:ResourceBank2023@cluster0.yn9mlqo.mongodb.net/Users", {useUnifiedTopology: true})
-.then(() => console.log("Connection Successfull!"))
-.catch((err) => console.log(err));
